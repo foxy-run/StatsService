f5c4299bfb9bd095b4bee5e07298a6f1bb36aa4b
Xenia Bocharova <bochxenia@yandex.ru>
2020-08-31 17:28:12 +0300

    Add 4 method

--- src/main/java/ru/netology/stats/StatsService3.java
+++ src/main/java/ru/netology/stats/StatsService3.java
@@ -1,4 +1,15 @@
 package ru.netology.stats;
 
 public class StatsService3 {
+    public long calculateFindMin(long[] sales) {
+        long currentMax = sales[0];
+
+        for (long sale : sales) {
+            if (currentMax > sale) {
+
+                currentMax = sale;
+            }
+        }
+        return currentMax;
+    }
 }
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -30,6 +30,13 @@ class StatsServiceTest {
 
         System.out.println(service.calculateFindMax(sales));
     }
+    @Test
+    void calculateFindMin() {
 
+        StatsService3 service = new StatsService3();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        System.out.println(service.calculateFindMin(sales));
+    }
 
 }
